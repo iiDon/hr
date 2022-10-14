38b8e157896d07a4675514eed104679711536058
Sultan <abu-farris@hotmail.com>
2022-10-14 17:50:05 +0300

    add the structrue for add job page

--- pages/new-job.tsx
+++ pages/new-job.tsx
@@ -1,7 +1,48 @@
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+  VStack,
+} from "@chakra-ui/react";
 import React from "react";
 
 const NewJob = () => {
-  return <div>new-job</div>;
+  return (
+    <VStack py="5rem" px="2rem" border="2px">
+      <FormControl>
+        <FormLabel>
+          <Box as="span" textColor="red">
+            *
+          </Box>
+          Job Name:
+        </FormLabel>
+        <Input type="text" />
+        <FormLabel>
+          <Box as="span" textColor="red">
+            *
+          </Box>
+          Job Descreiption:
+        </FormLabel>
+        <Textarea />
+        <Flex>
+          <Box w="50%">
+            <FormLabel>Salary:</FormLabel>
+            <Input type="number" />
+          </Box>
+          <Box w="50%">
+            <FormLabel>Salary:</FormLabel>
+            <Input type="number" />
+          </Box>
+        </Flex>
+
+        <Button>Submit</Button>
+      </FormControl>
+    </VStack>
+  );
 };
 
 export default NewJob;
