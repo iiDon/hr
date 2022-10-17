21120bad6fc593e0dd506b24e556778b622f7aa7
Sultan <abu-farris@hotmail.com>
2022-10-17 16:11:09 +0300

    finish new job page

---
+++ components/New-Job/Dropdown.tsx
@@ -0,0 +1,41 @@
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import { Menu, MenuButton, Button, MenuList, MenuItem } from "@chakra-ui/react";
+import { ChangeEvent, useState } from "react";
+interface DropdownProps {
+  setJobState: (value: string) => void;
+}
+
+const Dropdown = ({ setJobState }: DropdownProps) => {
+  const menu = ["Public", "Draft"];
+  const [placeholder, setPlaceholder] = useState("Public");
+
+  const handleMenu = (item: string) => {
+    setJobState(item);
+    setPlaceholder(item);
+  };
+
+  return (
+    <>
+      <Menu>
+        <MenuButton
+          as={Button}
+          rightIcon={<ChevronDownIcon />}
+          w="100%"
+          border="1px"
+          borderColor="black"
+        >
+          {placeholder}
+        </MenuButton>
+        <MenuList w="100%">
+          {menu.map((item) => (
+            <MenuItem onClick={() => handleMenu(item)} key={item}>
+              {item}
+            </MenuItem>
+          ))}
+        </MenuList>
+      </Menu>
+    </>
+  );
+};
+
+export default Dropdown;
--- pages/new-job.tsx
+++ pages/new-job.tsx
@@ -1,51 +1,133 @@
 import {
-  Box,
   Button,
   Flex,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Input,
   Textarea,
+  useToast,
   VStack,
 } from "@chakra-ui/react";
-import React from "react";
+import Dropdown from "../components/New-Job/Dropdown";
+import { useFormik } from "formik";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 const NewJob = () => {
-  return (
-<VStack py="5rem" px="2rem" w='100%'>
-      <FormControl>
-        <FormLabel>
-          <Box as="span" textColor="red">
-            *
-          </Box>
-          Job Name:
-        </FormLabel>
-        <Input type="text" border='1px'  borderColor='black' />
-        <FormLabel>
-          <Box as="span" textColor="red">
-            *
-          </Box>
-          Job Descreiption:
-        </FormLabel>
-        <Textarea border='1px'  borderColor='black'/>
-        <Flex>
-          <Box  w="50%" pr={2}>
-            <FormLabel>Type:</FormLabel>
-            <Input  type="number" border='1px'  borderColor='black'/>
-          </Box>
-          <Box w="50%" pl={2}>
-            <FormLabel>Salary:</FormLabel>
-            <Input type="number" border='1px'  borderColor='black'/>
-          </Box>
-          <Box w="50%" pl={2}>
-            <FormLabel>Status:</FormLabel>
-            <Input type="number" border='1px'  borderColor='black'/>
-          </Box>
-        </Flex>
+  const router = useRouter();
+  const [jobState, setJobState] = useState("Public");
+  const toast = useToast();
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      description: "",
+      startDate: new Date().toISOString().split("T")[0],
+      endDate: new Date().toISOString().split("T")[0],
+      salary: "",
+      type: "",
+      state: jobState,
+    },
+    onSubmit: (values) => {
+      values.state = jobState;
+      router.push("/jobs");
+      toast({
+        title: "Job Added",
+        status: "success",
+        duration: 6000,
+        isClosable: false,
+      });
+    },
+  });
 
-        <Button bgColor='blue.900' textColor='white' w='100%' mt='1rem'>Submit</Button>
-      </FormControl>
-    </VStack>
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <VStack py="5rem" px="2rem" w="100%">
+        <FormControl isRequired>
+          <FormLabel htmlFor="title">Job Title:</FormLabel>
+          <Input
+            name="title"
+            id="title"
+            type="text"
+            border="1px"
+            borderColor="black"
+            onChange={formik.handleChange}
+            value={formik.values.title}
+          />
+          <FormErrorMessage>Please Enter Title</FormErrorMessage>
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel htmlFor="description">Job Description:</FormLabel>
+          <Textarea
+            border="1px"
+            borderColor="black"
+            onChange={formik.handleChange}
+            // value={formik.values.description}
+          />
+          <FormErrorMessage>Please Enter Description</FormErrorMessage>
+        </FormControl>
+        <Flex w="100%">
+          <FormControl pr="1rem" isRequired>
+            <FormLabel htmlFor="startDate">Start Date:</FormLabel>
+            <Input
+              min={new Date().toISOString().split("T")[0]}
+              // disabled={checkDate(formik.values.endDate)}
+              name="startDate"
+              id="startDate"
+              type="date"
+              onChange={formik.handleChange}
+              value={formik.values.startDate}
+              border="1px"
+              borderColor="black"
+            />
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel htmlFor="endDate">End Date:</FormLabel>
+            <Input
+              min={new Date().toISOString().split("T")[0]}
+              type="date"
+              name="endDate"
+              id="endDate"
+              onChange={formik.handleChange}
+              value={formik.values.endDate}
+              border="1px"
+              borderColor="black"
+            />
+          </FormControl>
+        </Flex>
+        <Flex w="100%">
+          <FormControl pr="1rem">
+            <FormLabel>Type:</FormLabel>
+            <Input type="number" border="1px" borderColor="black" />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Salary:</FormLabel>
+            <Input
+              type="number"
+              border="1px"
+              borderColor="black"
+              id="salary"
+              name="salary"
+              onChange={formik.handleChange}
+              value={formik.values.salary}
+            />
+          </FormControl>
+        </Flex>
+        <FormControl isRequired>
+          <Dropdown setJobState={setJobState} />
+        </FormControl>
+
+        <Button
+          type="submit"
+          bgColor="blue.900"
+          textColor="white"
+          w="100%"
+          mt="1rem"
+        >
+          Submit
+        </Button>
+      </VStack>
+    </form>
   );
 };
 
